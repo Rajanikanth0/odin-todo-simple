76571accebd9ff94472b5278a1ec4db6476c7699
Rajanikanth0 <61695662+Rajanikanth0@users.noreply.github.com>
2025-12-28 11:06:54 +0530

    feat: add new-project and save data

--- src/tab/loadProjects.js
+++ src/tab/loadProjects.js
@@ -1,5 +1,6 @@
 import { createElement } from "../modules/utility.js";
 import { getStorageData } from "../modules/storage.js";
+import { Project } from "../modules/project.js";
 
 function getProjectElement(project) {
   const p = createElement("p", {
@@ -11,9 +12,70 @@ function getProjectElement(project) {
   return p;
 }
 
+function getFormData(e) {
+  e.preventDefault();
+  
+  const formData = new FormData(e.target);
+  const data = Array.from( formData.values() );
+
+  const project = new Project(...data);
+  project.addData();
+
+  renderProjects();
+}
+
+function getProjectForm() {
+  const name = createElement("input");
+  name.setAttribute("name", "name");
+  name.setAttribute("placeholder", "Name...");
+
+  const description = createElement("input");
+  description.setAttribute("name", "description");
+  description.setAttribute("placeholder", "Description...");
+
+  const date = createElement("input");
+  date.setAttribute("type", "date");
+  date.setAttribute("name", "date");
+
+  const button = createElement("button", {text: "Save"});
+  button.setAttribute("type", "submit");
+
+  const form = createElement("form");
+  form.addEventListener("submit", getFormData);
+
+  form.append(name, description, date, button);
+
+  return form;
+}
+
+function renderCreateProject(e) {
+  const target = e.target;
+  const targetParent = target.parentElement;
+  targetParent.removeChild(target);
+
+  const formElement = getProjectForm();
+  
+  targetParent.appendChild(formElement);
+}
+
+function getCreateButton() {
+  const createProjectContainer = createElement("div", { classes: ["createProjectContainer"]});
+
+  const createProjectButton = createElement("button", {
+    classes: ["createProjectButton"],
+    text: "New Project"
+  });
+
+  createProjectButton.addEventListener("click", renderCreateProject);
+
+  createProjectContainer.appendChild(createProjectButton);
+  return createProjectContainer;
+}
+
 function renderProjects() {
   const projects = Object.values( getStorageData() );
   const projectLabels = createElement("div", { classes: ["projectLabels"] });
+  const createButton = getCreateButton();
 
   for (const project of projects) {
     projectLabels.appendChild( getProjectElement(project) );
@@ -21,7 +83,7 @@ function renderProjects() {
 
   const content = document.querySelector(".content");
   content.textContent = "";
-  content.appendChild(projectLabels);
+  content.append(projectLabels, createButton);
 }
 
 export { renderProjects };
